da0d47bebe3747d14fd2065950f69604a82d3378
shouda <shouda@gmail.com>
2016-04-28 18:49:13 +0800

    remove global .babelrc and node6 preset, only transform es2015 modules import when run koa server

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,10 @@ const config = {
         exclude: [path.resolve(__dirname, 'node_modules')],
         include: [path.resolve(__dirname, 'src')],
         loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['react-hot-loader/babel'],
+        },
       },
     ],
   },
@@ -88,7 +92,7 @@ const config = {
       require('postcss-import')({ addDependencyTo: webpack }),
       require('postcss-url')(),
       require('postcss-cssnext')(),
-      // add your "plugins" here
+      // add your 'plugins' here
       // ...
       // and if you want to compress,
       // just use css-loader option that already use cssnano under the hood
