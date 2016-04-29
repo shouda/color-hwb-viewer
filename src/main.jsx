c3261cfcf17d3d2e6bdb36c54400ad8ea51f8a56
shouda <shouda@gmail.com>
2016-04-29 17:20:22 +0800

    pass hmrKey to Router only at development

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import { configureRoute } from './routes/configureRoute';
-
-const hmrKey = Math.random();
+import getRouter from './routes/configureRoute';
 
 const Main = ({ store, history }) => (
   <Provider store={store}>
-    <Router key={hmrKey} history={history} routes={configureRoute} />
+    {getRouter(history)}
   </Provider>
 );
 
--- src/routes/configureRoute.js
+++ src/routes/configureRoute.js
@@ -1,24 +1,35 @@
+import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
 import App from '../layouts/App';
 import Hwb from '../containers/Hwb';
 
-export const configureRoute = {
-  path: '/',
-  component: App,
-  indexRoute: { component: Hwb },
-  childRoutes: [
-    {
-      path: 'picked',
-      component: Hwb,
-      childRoutes: [
-        {
-          path: 'picked/:hex',
-          component: Hwb,
-        },
-      ],
-    },
-    {
-      path: '*',
-      component: Hwb,
-    },
-  ],
-};
+function getRoute() {
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={Hwb} />
+      <Route path="picked" component={Hwb}>
+        <Route path="picked/:hex" component={Hwb} />
+      </Route>
+      <Route path="*" component={Hwb} />
+    </Route>
+  );
+}
+
+export default function getRouter(history) {
+  let router;
+  if (module.hot) {
+    const hmrKey = Math.random();
+    router = (
+      <Router key={hmrKey} history={history}>
+        {getRoute()}
+      </Router>
+    );
+  } else {
+    router = (
+      <Router history={history}>
+        {getRoute()}
+      </Router>
+    );
+  }
+  return router;
+}
