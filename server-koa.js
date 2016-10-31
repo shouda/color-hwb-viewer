a007e26b715fcf55c09cf81d620b36c471737859
shouda <shouda@gmail.com>
2016-10-31 17:42:23 +0800

    upgrade to koa 2

--- server-koa.js
+++ server-koa.js
@@ -2,14 +2,21 @@
 
 'use strict';
 
-const koa = require('koa');
+const Koa = require('koa');
 const serve = require('koa-static');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('koa-webpack-dev-middleware');
 const webpackHotMiddleware = require('koa-webpack-hot-middleware');
 const webpackConfig = require('./webpack.config');
 
-const app = koa();
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`); // eslint-disable-line no-console
+});
 
 let viewDir;
 if (process.env.NODE_ENV !== 'production') {
