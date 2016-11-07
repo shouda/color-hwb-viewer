82911d3afc5f71670e197a853d6079009b2b4569
shouda <shouda@gmail.com>
2016-11-07 23:23:35 +0800

    enable new-cap

--- eslint.js
+++ eslint.js
@@ -4,7 +4,6 @@ module.exports = {
     // require.resolve('./other-rules.js'),
   ],
   rules: {
-    'new-cap': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'jsx-a11y/no-static-element-interactions': 0,
   },
