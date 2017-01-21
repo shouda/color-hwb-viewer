033182951c6837c6803335f3876adfe02ae23bf4
shouda <shouda@gmail.com>
2017-01-21 14:36:37 +0800

    upgrade to es2015

--- script/chunkhash.js
+++ script/chunkhash.js
@@ -5,15 +5,12 @@ const path = require('path');
 
 const jsonContent = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../dist/stats.json')));
 
-/* eslint-disable no-var, prefer-template */
-var htmlContent = fs.readFileSync(path.resolve(__dirname, '../src/index.html')).toString();
+let htmlContent = fs.readFileSync(path.resolve(__dirname, '../src/index.html')).toString();
 
 htmlContent = htmlContent.replace(
   ' src="./vendors.js"',
-  ' src="./' + jsonContent.assetsByChunkName.vendors[0] + '"'
-);
+  ` src="./${jsonContent.assetsByChunkName.vendors[0]}"`);
 htmlContent = htmlContent.replace(
   ' src="./app.js"',
-  ' src="./' + jsonContent.assetsByChunkName.app[0] + '"'
-);
+  ` src="./${jsonContent.assetsByChunkName.app[0]}"`);
 fs.writeFileSync(path.resolve(__dirname, '../dist/index.html'), htmlContent);
