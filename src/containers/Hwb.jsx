b1f13729abf787b98e0d03b28182f3c25a97eb14
shouda <shouda@gmail.com>
2017-02-13 17:34:27 +0800

    upgrade to react-router-dom v4 beta

--- src/containers/Hwb.jsx
+++ src/containers/Hwb.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import {
   selectHue,
   addHue,
