6b324e0597b9ff50dd033ff42efb66291f4d10f4
shouda <shouda@gmail.com>
2017-02-13 17:23:18 +0800

    upgrade to react-router-dom v4 beta

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { HashRouter, Match, Miss } from 'react-router';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Hwb from '../containers/Hwb';
 
 const App = () => (
-  <HashRouter>
+  <Router>
     <div>
       <div className="clearfix mx-auto" style={{ width: '13em' }}>
         <h2>HWB Color Space</h2>
       </div>
       <div className="clearfix mx-auto" style={{ width: '90%' }}>
-        <Match pattern="/" exactly component={Hwb} />
-        <Match pattern="/picked/:hex" component={Hwb} />
-        <Miss component={Hwb} />
+        <Switch>
+          <Route path="/" exact component={Hwb} />
+          <Route path="/picked/:hex" component={Hwb} />
+          <Route component={Hwb} />
+        </Switch>
       </div>
     </div>
-  </HashRouter>
+  </Router>
 );
 
 export default App;
